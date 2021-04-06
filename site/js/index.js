7a2e65a40da4ec904fce492a8da370336d1093fa
hcaeryks <skyr34chl@gmail.com>
2021-04-05 23:45:51 -0300

    adicionado página do remédio

--- site/js/index.js
+++ site/js/index.js
@@ -4,16 +4,29 @@ botoesCategorias = categoriasDiv.querySelectorAll("button")
 remediosDiv = document.getElementById("remedios")
 tituloPaginaRemedios = document.getElementById("tituloRemedios")
 
+remedioDiv = document.getElementById("remedio")
+tituloPaginaRemedio = document.getElementById("tituloRemedio")
+
+campoPesquisa = document.getElementById("floatingInput")
+
 window.onload = function() {
 	for(var i = 0; i < botoesCategorias.length; i++) {
 		botoesCategorias[i].onclick = function() {
-			mudarPaginaCategoria(this);
+			mudarPaginaCategoria(this)
 		}
 	}
 }
 
+campoPesquisa.addEventListener("keyup", function(e) {
+	if(e.keyCode === 13) {
+		e.preventDefault()
+		carregarPesquisa(campoPesquisa.value)
+	}
+})
+
 function mudarPaginaCategoria(botao) {
 	categoriasDiv.classList.add("escondido")
+	remedioDiv.classList.add("escondido")
 	tituloPaginaRemedios.innerHTML = " Categoria: " + botao.innerHTML
 	setTimeout(() => {
 		remediosDiv.classList.remove("escondido")
@@ -22,9 +35,23 @@ function mudarPaginaCategoria(botao) {
 
 function voltarInicio() {
 	remediosDiv.classList.add("escondido")
+	remedioDiv.classList.add("escondido")
 	setTimeout(() => {
 		categoriasDiv.classList.remove("escondido")
 	}, 250)
 }
 
-autocomplete(document.getElementById("floatingInput"), remediosTemporario)
+function carregarPesquisa(pesquisa) {
+	if(remediosTemporario.includes(pesquisa)) {
+		remediosDiv.classList.add("escondido")
+		categoriasDiv.classList.add("escondido")
+		tituloPaginaRemedio.innerHTML = " " + pesquisa
+		setTimeout(() => {
+			remedioDiv.classList.remove("escondido")
+		}, 250)
+	} else {
+
+	}
+}
+
+autocomplete(campoPesquisa, remediosTemporario)
