bfecbf3e9da24427ac065fce908d7ff9299ade23
OverMan-647 <54404011+OverMan-647@users.noreply.github.com>
2021-04-07 23:22:42 -0300

    Adicionado função para procurar remedios pela categoria

--- api/main.js
+++ api/main.js
@@ -1,3 +1,5 @@
+//npm install --save-dev nodemon
+//npm intall mysql
 const mysql = require('mysql');
 //para acessar o banco de dados utilize a variavel "database"
 const database = mysql.createConnection({
@@ -6,7 +8,17 @@ const database = mysql.createConnection({
   password: 'rTbWkXuxr6',
   database: 'LUtMU4ebQM'
 });
-connection.connect((err) => {
+database.connect((err) => {
   if (err) throw err;
-  console.log('Connected!');
 });
+
+//Retorna uma lista com os remedios de determinada categoria
+function busca_categoria(categoria) {
+  database.query("SELECT * FROM `categoria` WHERE nome = '" + categoria + "'", (err,rows) => {
+  if(err) throw err;
+  console.log('Data received from Db:');
+  console.log(rows);
+  id_categoria = rows.id
+  return database.query("SELECT * FROM `remedio` WHERE categoria = '" + id_categoria + "'")
+});
+}
